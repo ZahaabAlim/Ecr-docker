6ebaffe5d61059844426e86c6997d83dc1c219fe
ZahaabAlim <Zahaabalim@gmail.com>
2024-02-13 15:02:20 +0530

    added to workflow

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
+# Configure the AWS provider
 provider "aws" {
   region = "us-east-1"  
 }
+# Configure the Terraform backend to store the state in S3 and use DynamoDB for locking
 terraform {
   backend "s3" {
     bucket = "tf-backend-st"
@@ -10,11 +12,11 @@ terraform {
     dynamodb_table = "TfStatelock"
   }
 }
- 
+# Create an ECR repository 
 resource "aws_ecr_repository" "my_ecr_repo" {
   name = "my-ecr-repo"
 }
- 
+# Create an ECR lifecycle policy 
 resource "aws_ecr_lifecycle_policy" "my_ecr_policy" {
   repository = aws_ecr_repository.my_ecr_repo.name
  
