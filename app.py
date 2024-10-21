e7662ae449b3c863395dc7572638091acaf548d8
ZahaabAlim <150016924+ZahaabAlim@users.noreply.github.com>
2024-10-21 11:42:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello Zahaab Alim")
+import sys
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python factorial.py <number>")
+    else:
+        number = int(sys.argv)
+        result = factorial(number)
+        print(f"The factorial of {number} is {result}")
