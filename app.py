21e16870e846dc1168dbc350b89dad63b00e6e45
ZahaabAlim <150016924+ZahaabAlim@users.noreply.github.com>
2024-10-21 12:32:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python factorial.py <number>")
     else:
-        number = int(sys.argv)
+        number = int(sys.argv[1])  # Corrected this line
         result = factorial(number)
         print(f"The factorial of {number} is {result}")
